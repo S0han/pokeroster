ea5f70bf0e04ef49a84c36dba5466486ede2458d
Kane Pereira <kane.pereira18@gmail.com>
2024-01-28 22:07:23 -0500

    create a slot component which will hold the pokeon sprite and data

---
+++ app/components/slot.component.jsx
@@ -0,0 +1,7 @@
+export default function Slot() {
+    return  (
+        <div>
+            
+        </div>
+    )
+}
--- app/page.js
+++ app/page.js
@@ -1,9 +1,13 @@
 import Image from "next/image";
 
-export default function Home() {
+export default function App() {
   return (
     <main>
-      
+      <title>Pokeroster</title>
+      <body>
+        <h1>Pokeroster</h1>
+
+      </body>
     </main>
   );
 }
