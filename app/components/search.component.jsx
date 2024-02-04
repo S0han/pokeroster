e39f76fde440c02dd0209753b379df86687c86df
Kane Pereira <kane.pereira18@gmail.com>
2024-02-03 20:34:30 -0500

    add log to update the state of the input text box

--- app/components/search.component.jsx
+++ app/components/search.component.jsx
@@ -1,11 +1,21 @@
+'use client';
+import { useState, useEffect } from 'react'; 
 import Slot from './slot.component'
 
 const Search = () => {
+
+    const [inputValue, setInputValue] = useState('');
+
+    const searchInputHandler = (event) => {
+        setInputValue(event.target.value);
+        console.log(inputValue);
+    }
+
     return (
         <div>
             <form>
                 <label> Enter Pokemon name:</label>
-                <input type="text" id="name_input" />
+                <input type="text" id="name_input" value={inputValue} onChange={searchInputHandler} />
                 <input type="submit" />
             </form>
             <Slot />
