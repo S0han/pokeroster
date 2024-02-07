78eda222b7c95d1d01bd94ee2a88c7658bd0ff4d
Kane Pereira <kane.pereira18@gmail.com>
2024-02-07 15:08:21 -0500

    set up body-parser on server.js side

--- app/server.js
+++ app/server.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 const PORT = 3001;
 
-app.use(express.json());
+//body parser middleware setup
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.listen(PORT, () => console.log(`App is listening on port ${PORT}.`));
-
+//populate table on database with roster data sent
 app.post('/submit-roster', (req, res) => {
     console.log(req.body);
 });
+
+
+app.listen(PORT, () => console.log(`App is listening on port ${PORT}.`));
+
