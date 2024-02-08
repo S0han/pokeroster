2d6a978f024c08b380a30d6ddc01bbac054bd991
Kane Pereira <kane.pereira18@gmail.com>
2024-02-08 08:25:19 -0500

    fix cors error by adding corsOptions

--- app/server.js
+++ app/server.js
@@ -5,16 +5,19 @@ const cors = require('cors');
 const app = express();
 const PORT = 3001;
 
+let corsOptions = { 
+    origin : ['http://localhost:3001'], 
+ }
 
 //CORS middleware
-app.use(cors());
+app.use(cors(corsOptions));
 
 //body parser middleware setup
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 //populate table on database with roster data sent
-app.post('http://localhost:3001/submit-roster', (req, res) => {
+app.post('http://localhost:3000/submit-roster', (req, res) => {
     console.log(req.body);
     res.json({ message: "Roster submitted successfully" });
 });
