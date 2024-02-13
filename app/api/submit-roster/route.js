12ebc692d729c7a2f3fdee7d140f64e17e7329d8
Kane Pereira <kane.pereira18@gmail.com>
2024-02-13 01:37:16 -0500

    successful POST request with correct reponse code 200

--- app/api/submit-roster/route.js
+++ app/api/submit-roster/route.js
@@ -7,12 +7,12 @@ export async function POST(req, res) {
         const result = await addRoster([pokemon1, pokemon2, pokemon3, pokemon4, pokemon5, pokemon6]);
         console.log(result);
         
-        return new NextResponse.json('Roster Submitted Successfully', {
+        return NextResponse.json('Roster Submitted Successfully', {
             status: 200,
         });
 
     } catch (e) {
-        return new NextResponse.json(`Webhook error: ${e.message}`, {
+        return NextResponse.json(`Webhook error: ${e.message}`, {
             status: 400,
         });
     }
