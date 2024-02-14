ed5768ffb5a61d3495c18ef3e2c5fa93a3714aa8
Kane Pereira <kane.pereira18@gmail.com>
2024-02-14 00:18:44 -0500

    setting up navbar routes

--- app/components/navbar.component.jsx
+++ app/components/navbar.component.jsx
@@ -2,7 +2,7 @@ const Navbar = () => {
     return (
         <div className='Navbar'>
             <a href='/'>Home</a>
-            <a href='./pages/top3pokemon'>Top 3 Pokemon</a>
+            <a href='../pages/top3pokemon.js'>Top 3 Pokemon</a>
         </div>
     );
 }
