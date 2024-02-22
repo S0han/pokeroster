e074ad875b4f36043dad407ac3a52ae37a894086
Kane Pereira <kane.pereira18@gmail.com>
2024-02-22 16:36:13 -0500

    add tailwind

---
+++ prisma/migrations/20240212052248_rosters/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "rosters" (
+    "id" SERIAL NOT NULL,
+    "pokemon1" TEXT,
+    "pokemon2" TEXT,
+    "pokemon3" TEXT,
+    "pokemon4" TEXT,
+    "pokemon5" TEXT,
+    "pokemon6" TEXT,
+
+    CONSTRAINT "rosters_pkey" PRIMARY KEY ("id")
+);
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export const content = [
+  './app/**/*.{js,ts,jsx,tsx,mdx}',
+  './app/page.js',
+];
+export const theme = {
+  extend: {}
+};
+export const plugins = [
+  'tailwindcss',
+  'autoprefixer'
+];
+
